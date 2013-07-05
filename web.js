b3971d887898a482910787afb6ded9f50e1cb6ae
Tim Bentley <tim.bentley@gmail.com>
2013-07-05 11:53:00 -0400

    new files

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2');
+    var k = fs.readFileSync('./index.html','utf8');
+    response.send( k.toString() );
 });
 
 var port = process.env.PORT || 5000;
